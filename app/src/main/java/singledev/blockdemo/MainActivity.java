8f81636885c351e3f10c2f006a237d210edf2b97
prakashbalab24 <prakashbala.b24@gmail.com>
2018-04-03 12:56:36 +0530

    added intent

--- app/src/main/java/singledev/blockdemo/MainActivity.java
+++ app/src/main/java/singledev/blockdemo/MainActivity.java
@@ -1,11 +1,15 @@
 package singledev.blockdemo;
 
 import android.app.ActivityManager;
+import android.app.AppOpsManager;
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Handler;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,27 +24,34 @@ import java.util.TreeMap;
 public class MainActivity extends AppCompatActivity {
     private Button startBtn,stopBtn;
     private Handler handler;
+    private Boolean isRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         startBtn = findViewById(R.id.startBtn);
         stopBtn = findViewById(R.id.stopBtn);
         handler = new Handler();
-        final int delay = 5000; //milliseconds
+        final int delay = 3000; //milliseconds
 
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (isRunning){
+                    return;
+                }
                 handler.postDelayed(new Runnable(){
                     public void run(){
                         printForegroundTask();
                         handler.postDelayed(this, delay);
                     }
                 }, delay);
+                isRunning = true;
                 //startService();
-                Toast.makeText(MainActivity.this,"Started",Toast.LENGTH_LONG).show();
+
             }
         });
 
@@ -48,10 +59,39 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 handler.removeCallbacksAndMessages(null);
+                isRunning = false;
                 //stopService();
             }
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(!isAccessGranted()){
+            Toast.makeText(this,"Please turn on usage access",Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+            startActivity(intent);
+            return;
+        }
+    }
+
+    private boolean isAccessGranted() {
+        try {
+            PackageManager packageManager = getPackageManager();
+            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(getPackageName(), 0);
+            AppOpsManager appOpsManager = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
+            int mode = 0;
+            if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.KITKAT) {
+                mode = appOpsManager.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
+                        applicationInfo.uid, applicationInfo.packageName);
+            }
+            return (mode == AppOpsManager.MODE_ALLOWED);
+
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
     }
 
     public void startService() {
@@ -62,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void printForegroundTask() {
+        Toast.makeText(MainActivity.this,"RUNNING...",Toast.LENGTH_LONG).show();
         String currentApp = "NULL";
         if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
             UsageStatsManager usm = (UsageStatsManager) this.getSystemService(Context.USAGE_STATS_SERVICE);
